ced798d208693bc856b1a602e2b822f3c04ea7ce
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2025-06-28 14:39:37 +0300

    Implement user management routes and update database schema to use UUIDs as primary keys

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
-
+import userRouter from './routes/userRoutes';
 const app = express();
 
 app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
+app.use('/api/users', userRouter);
+
 
 const PORT = process.env.PORT || 3000;
 
